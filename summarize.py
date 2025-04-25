720a7c466d3fe36763e67b139efb6e544c0f592b
Vishal-Bysani <vishal.bysani99@gmail.com>
2025-04-26 01:03:43 +0530

    Change model path

--- summarize.py
+++ summarize.py
@@ -1,5 +1,6 @@
 # summarize.py
 import torch
+import sys
 from transformers import pipeline,AutoTokenizer, AutoModelForSeq2SeqLM
 
 # model = AutoModelForSeq2SeqLM.from_pretrained("facebook/bart-large-cnn", cache_dir = "./models/fbcnn")
@@ -7,14 +8,14 @@ from transformers import pipeline,AutoTokenizer, AutoModelForSeq2SeqLM
 # model.save_pretrained("./models/fbcnn")
 # tokenizer.save_pretrained("./models/fbcnn")
 device = 0 if torch.cuda.is_available() else -1
-summarizer = pipeline("summarization", model="./models/fbcnn", tokenizer="./models/fbcnn", device=device)
+summarizer = pipeline("summarization", model="../models/fbcnn", tokenizer="../models/fbcnn", device=device)
 
 
 def summarize(text):
-    result = summarizer(text, max_length=1000, min_length=0, do_sample=False)
+    result = summarizer(text, max_length=100, min_length=0, do_sample=False)
     return result[0]['summary_text']
 
 if __name__ == "__main__":
-    input_text = input()
+    input_text = sys.stdin.read()
     summary = summarize(input_text)
     print(summary)
